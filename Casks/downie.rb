9aaf62f66223ada836b9e938067d43c739c25492
DeX <dxu101@gmail.com>
2023-03-17 11:22:37 +0100

    downie

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,14 @@
 cask "downie" do
+<<<<<<< HEAD
   version "4.6.10,4528"
   sha256 "1599c5a835c0664bd8fe4aebfe964ea5391c689c77cd33a6d9a13928eb38e459"
+||||||| parent of 28a44313be (downie)
+  version "4.6.7,4516"
+  sha256 "e426b8ce51280bdd39ed764e45bc12d5f4a992fa693a9beca333494b7ef3cbcd"
+=======
+  version "4.6.11,4533"
+  sha256 "752d17a5185d8770289a3548dc77b0d3b0f4c8e6afbd6108871c542e04a75933"
+>>>>>>> 28a44313be (downie)
 
   url "https://software.charliemonroe.net/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.csv.second}.dmg"
   name "Downie"
